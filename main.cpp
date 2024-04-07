2f1c74176117b9d5a56aa4cdcbbc9c1796818f83
pripri-plays <166307045+pripri-plays@users.noreply.github.com>
2024-04-07 17:02:59 +0300

    Added main files
    Added the main C++ file and an executable file for download
    
    [Log] Version 1.0 has been created

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+class Calculator {
+public:
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double subtract(double a, double b) {
+        return a - b;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            std::cout << "Error: Division by zero" << std::endl;
+            return 0;
+        }
+    }
+};
+
+int main() {
+    Calculator calc;
+    double num1, num2;
+    char op;
+
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    double result;
+    switch (op) {
+        case '+':
+            result = calc.add(num1, num2);
+            break;
+        case '-':
+            result = calc.subtract(num1, num2);
+            break;
+        case '*':
+            result = calc.multiply(num1, num2);
+            break;
+        case '/':
+            result = calc.divide(num1, num2);
+            break;
+        default:
+            std::cout << "Invalid operator" << std::endl;
+            return 1;
+    }
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
